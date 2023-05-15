7d45cbb0a03098cf5cccfe90197557e310897329
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-05-15 08:47:33 -0300

    ATUALIZAÇÃO

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+        include('conexao.php');
+
+        if(!isset($_SESSION)){
+            session_start();
+        }
+    ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -10,8 +18,49 @@
     </head>
     <body>
         <div class="container">
-            <h1>Buteco do Nunes</h1>
-                <div class="card" style="width: 18rem;">
+        <nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Buteco do Nunes</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Buteco do Nunes</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Início</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Combos</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Informações
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark">
+              <li><a class="dropdown-item" href="#">Sobre nós</a></li>
+              <li><a class="dropdown-item" href="#">Login</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Contato</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex mt-3" role="search">
+          <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
+          <button class="btn btn-success" type="submit">Pesquisar</button>
+        </form>
+      </div>
+    </div>
+  </div>
+    </nav>
+        <h1>Destaques</h1>
+                <div  class="card" style="width: 18rem;">
                     <img src="img/asset-generation-fc12b65e-7fc6-4ee2-b139-16f3eb7d1ef4-1-watermarked.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">X-nunes</h5>
@@ -19,7 +68,7 @@
                         <a href="pedido.php" class="btn btn-primary">Faça seu pedido</a>
                     </div>
                     </div>
-        </div>
+    </div>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </html>
